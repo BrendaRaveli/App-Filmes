972f7c5471c525002dc78697c138c6021d5f6998
Brenda Raveli <brenda.raveli@segimob.local>
2022-01-25 15:17:21 -0300

    commit inicial

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Titulo")]
+        public string Title { get; set; }
+
+        [Display(Name = "Data de lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        
+        [Display(Name = "Genero")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Avaliação")]
+        public string Rating { get; set; }
+    }
+}
